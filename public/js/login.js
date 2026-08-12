e33d3f774d0dc82543e303fdea1c9a13d5db41db
jmmeleu <jm.meleu@gmail.com>
2026-08-11 21:50:00 -0300

    quinta commit

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,12 @@
+        const userTeste = "userTeste";
+        const senhaTeste = "senhaTeste";
+
         const passwordInput = document.getElementById('password');
         const toggleButton = document.getElementById('toggleBtn');
         const eyeIcon = document.getElementById('eyeIcon');
+        const usernameInput = document.getElementById('username');
+        const loginForm = document.getElementById('loginForm');
+        const loginError = document.getElementById('loginError');
 
         toggleButton.addEventListener('click', function () {
             // Verifica o tipo atual do input
@@ -16,5 +22,19 @@
                 passwordInput.setAttribute('type', 'password');
                 // Restaura o ícone original do olho aberto
                 eyeIcon.innerHTML = `<path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>`;
+            }
+        });
+
+        loginForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            const usuarioDigitado = usernameInput.value;
+            const senhaDigitada = passwordInput.value;
+
+            if (usuarioDigitado === userTeste && senhaDigitada === senhaTeste) {
+                loginError.style.display = 'none';
+                window.location.href = '/';
+            } else {
+                loginError.style.display = 'block';
             }
         });
