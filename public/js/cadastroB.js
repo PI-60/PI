ccdd0c36835a7e18837a9869dfb2693a15619f00
heloreisch <heloisa.mafrat@gmail.com>
2026-08-23 15:33:01 -0300

    tela cadastro

--- public/js/cadastroB.js
+++ public/js/cadastroB.js
@@ -1,4 +1,6 @@
 
+
+// bolsista;
 document.addEventListener("DOMContentLoaded", function () {
     /*quando clicar nesse botão faça isso.. */
 
@@ -20,4 +22,57 @@ document.addEventListener("DOMContentLoaded", function () {
 
     });
 
+
 });
+
+    // paeticiapnte;
+
+    document.addEventListener("DOMContentLoaded", function () {
+    /*quando clicar nesse botão faça isso.. */
+
+    const tipoU = document.getElementById("tipoU");
+    const emailB = document.getElementById("phoneB");
+
+    console.log("Select:", tipoU);
+    console.log("Email:", emailB);
+
+    tipoU.addEventListener("change", function () {
+
+        console.log("Escolheu:", tipoU.value);
+
+        if (tipoU.value === "participante") {
+            emailB.style.display = "none";
+        } else {
+            phoneB.style.display = "flex";
+        }
+
+    });
+
+
+});
+
+// ministrante
+ document.addEventListener("DOMContentLoaded", function () {
+    /*quando clicar nesse botão faça isso.. */
+
+    const tipoU = document.getElementById("tipoU");
+    const cpfB = document.getElementById("cpfB");
+
+    console.log("Select:", tipoU);
+    console.log("CPF:", cpfB);
+
+    tipoU.addEventListener("change", function () {
+
+        console.log("Escolheu:", tipoU.value);
+
+        if (tipoU.value === "ministrante") {
+            emailB.style.display = "flex";
+        } else {
+            phoneB.style.display = "none";
+        }
+
+     });
+
+
+});
+    
